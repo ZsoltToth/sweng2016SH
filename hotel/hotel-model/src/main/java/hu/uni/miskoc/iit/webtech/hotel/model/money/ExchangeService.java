6660741712c7f2e4d58a1aee5dda5be758ba05c7
Zsolt Toth <tothzs@iit.uni-miskolc.hu>
2016-11-03 08:53:26 +0100

    ExchangeService has been renamed and its interface has been defined. It has to be implemented.

---
+++ hotel/hotel-model/src/main/java/hu/uni/miskoc/iit/webtech/hotel/model/money/ExchangeService.java
@@ -0,0 +1,22 @@
+package hu.uni.miskoc.iit.webtech.hotel.model.money;
+
+import java.util.Map;
+
+public class ExchangeService {
+	
+		/**
+		 * exchangeRatios[From,To] -> ratio
+		 */
+		private Map<Currency, Map<Currency, Double>> exchangeRatios;
+		
+		public ExchangeService(Map<Currency, Map<Currency, Double>> exchangeRatios) {
+			super();
+			this.exchangeRatios = exchangeRatios;
+		}
+
+
+
+		public Money exchange(Money currentDevisa, Currency targetCurrency){
+			return new Money(0, Currency.HUF);
+		}
+}
